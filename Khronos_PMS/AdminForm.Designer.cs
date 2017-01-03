a00f0804ea29bd0611fc8541dd6b64a2cc71170d
markostijak <marko.stijak@gmail.com>
2017-01-03 20:25:15 +0100

    First commit

--- Khronos_PMS/AdminForm.Designer.cs
+++ Khronos_PMS/AdminForm.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace Khronos_PMS {
-    partial class Form1 {
+    partial class AdminForm {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -25,10 +25,9 @@
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "AdminForm";
         }
 
         #endregion
     }
 }
-
--- Khronos_PMS/Program.cs
+++ Khronos_PMS/Program.cs
@@ -13,7 +13,7 @@ namespace Khronos_PMS {
         public static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
