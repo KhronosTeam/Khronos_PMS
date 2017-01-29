afa04a4253d2b380bcb881813015e5eb5c6f88a2
markostijak <marko.stijak@gmail.com>
2017-01-29 01:57:04 +0100

    UnitForm

---
+++ Khronos_PMS/View/UnitForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Khronos_PMS.View {
+    public partial class UnitForm : Form {
+        public UnitForm() {
+            InitializeComponent();
+            //todo UnitForm je forma za kreiranje i editovanje unita
+        }
+    }
+}
