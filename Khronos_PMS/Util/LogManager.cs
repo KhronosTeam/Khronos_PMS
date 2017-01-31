b876a9e437b98ecbdd0af9dfe4fab269583da57d
ngolubovic <gvindolanOcube@gmail.com>
2017-02-01 00:37:54 +0100

    writeToLog in LogManager

--- Khronos_PMS/Util/LogManager.cs
+++ Khronos_PMS/Util/LogManager.cs
@@ -11,13 +11,35 @@ namespace Khronos_PMS.Util
     {
         private static KhronosPMSEntities entities = new KhronosPMSEntities();
 
-        public static void writeToLog(string TableID, string OperationID, string Params) {
+        public static void writeToLog(string TableID, string OperationID, string Params)
+        {
             Log newLog = new Log();
             newLog.Date = DateTime.Now;
             newLog.OperationID = OperationID;
             newLog.Params = Params;
             newLog.UserID = LoginManager.LoggedUser.ID;
             newLog.TableID = TableID;
+
+            entities.Logs.Add(newLog);
+            entities.SaveChangesAsync();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="enteties">enteties objekat koji je korišten za čuvanje u bazu</param>
+        /// <param name="TableID">ime tabele koja je mijenjana</param>
+        /// <param name="OperationID">"insert" OR "update"</param>
+        /// <param name="Params">if "insert" then new ID else old values</param>
+        /// <param name="userID">Id trenutno prijavljenog korisnika</param>
+        public static void writeToLog(KhronosPMSEntities enteties, string TableID, string OperationID, string Params, int userID)
+        {
+            Log newLog = new Log();
+            newLog.Date = DateTime.Now;
+            newLog.OperationID = OperationID;
+            newLog.Params = Params;
+            newLog.UserID = LoginManager.LoggedUser.ID;
+            newLog.TableID = TableID;
+            newLog.UserID = userID;
 
             entities.Logs.Add(newLog);
             entities.SaveChangesAsync();
