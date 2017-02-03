6a349801c3ae8a7b4747593c2976d3d683c82100
markostijak <marko.stijak@gmail.com>
2017-02-03 23:02:31 +0100

    Unit changes

--- Khronos_PMS/Model/Unit.view.cs
+++ Khronos_PMS/Model/Unit.view.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Khronos_PMS.Model {
     public partial class Unit {
-        public bool HasChildren => Children.Any(u => u.ID != Ancestor.ID);
+        public bool HasChildren => Children.Any(unit => unit.Active);
 
-        public bool IsRoot => ID == Ancestor.ID;
+        public bool IsRoot => Ancestor == null;
 
         public List<Worker> Assignees => Workers.Where(w => w.Active).Select(worksOn => worksOn.AssignedTo.Worker).ToList();
     }
